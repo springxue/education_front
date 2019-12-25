73411682cbf5f4755268899b9e57c980db805670
380794360@qq.com <xue4855189>
2019-12-25 23:55:58 +0800

    完成上传头像功能

--- src/views/edu/teacher/form.vue
+++ src/views/edu/teacher/form.vue
@@ -21,6 +21,34 @@
         <el-input v-model="teacher.intro" :rows="10" type="textarea"/>
       </el-form-item>
       <!--讲师头像 :TODO      -->
+
+      <!-- 讲师头像 -->
+      <el-form-item label="讲师头像">
+
+        <!-- 头衔缩略图 -->
+        <pan-thumb :image="teacher.avatar"/>
+        <!-- 文件上传按钮 -->
+        <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像
+        </el-button>
+
+        <!--
+    v-show：是否显示上传组件
+    :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+    :url：后台上传的url地址
+    @close：关闭上传组件
+    @crop-upload-success：上传成功后的回调 -->
+        <image-cropper
+          v-show="imagecropperShow"
+          :width="300"
+          :height="300"
+          :key="imagecropperKey"
+          :url="BASE_API+'/eduService/oss/upload'"
+          field="file"
+          @close="close"
+          @crop-upload-success="cropSuccess"/>
+
+      </el-form-item>
+
       <el-form-item>
         <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
       </el-form-item>
@@ -30,6 +58,9 @@
 
 <script>
 import teacher from '@/api/teacher'
+import ImageCropper from '@/components/ImageCropper'
+import PanThumb from '@/components/PanThumb'
+
 const defaultFrom={
   name: '',
   sort: 0,
@@ -40,8 +71,13 @@ const defaultFrom={
 };
 
 export default {
+  //声明使用的额外组件
+  components: { ImageCropper, PanThumb },
   data() {
     return {
+      BASE_API:process.env.BASE_API,//接口API地址
+      imagecropperShow:false,
+      imagecropperKey:0,
       teacher:defaultFrom,
       saveBtnDisabled : false // 保存按钮是否禁用,
       }
@@ -131,7 +167,24 @@ export default {
              }).catch(response=>{
                 console.log("555")
       })
+    },
+    //关闭上传组件
+    close(){
+        //点击x，关闭弹框
+      this.imagecropperShow=false;
+      //重新加载上传组件
+      this.imagecropperKey=this.imagecropperKey+1
+    },
+    cropSuccess(data){
+      console.log(data);
+    //获取后台返回图片的地址，显示到页面中去
+      this.teacher.avatar=data.imgUrl;
+      this.imagecropperShow=false;
+      //重新加载上传组件
+      this.imagecropperKey=this.imagecropperKey+1
+
     }
+
   }
 }
 
